db783bebdbccd5760608bb46776f37af55ef88cd
chrisezedi <91887106+chrisezedi@users.noreply.github.com>
2023-10-28 16:56:29 +0100

    add api.getknowtifyd.com to allowed headers

--- getknowtifyd/settings.py
+++ getknowtifyd/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'api.getknowtifyd.com']
 
 if not DEBUG:
     ALLOWED_HOSTS = ['.getknowtifyd.com']
